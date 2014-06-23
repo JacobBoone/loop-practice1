ed0965206080ebed3951849e4a33b52a45596c6a
Jacob <jakeboone@aol.com>
2014-06-23 12:40:32 -0600

    Exercise completed

---
+++ main.js
@@ -0,0 +1,77 @@
+
+// Example
+console.log("The Example")
+for(var i=0; i<5; i++) {
+ 	console.log(i);
+}
+
+
+
+ // Problem 1
+console.log("Number One")
+
+ for(var i=0; i<501; i+=100) {
+ 	console.log(i);
+}
+
+
+// Problem 2
+console.log("Number two")
+ for(var i=1; i<65; i=i*2) {
+ 	console.log(i);
+}
+
+
+// Problem 3
+console.log("Number three")
+ for(var i=1; i<4; i++) {
+ 	console.log(i);
+ 	console.log(i);
+ 	console.log(i);
+}
+
+//  Problem 4
+console.log("Number four")
+
+for(var i=0; i<11; i+=2) {
+ 	console.log(i);
+}
+
+
+// Problem 5
+
+console.log("Number five")
+for(var i=3; i<16; i+=3) {
+ 	console.log(i);
+}
+
+for(var i=3; i<16; i+=3) {
+ 	console.log(i);
+}
+
+// problem 6
+console.log("Number six")
+
+for(var i=9; i>=0; i--) {
+ 	console.log(i);
+}
+
+
+// Problem 7
+
+console.log("Number Seven")
+
+
+for(var i=1; i<4; i++) {
+ 	for(var j=0; j<4; j++){
+ 		console.log(j)
+ 	
+ 	}
+}
+
+
+
+
+
+
+
